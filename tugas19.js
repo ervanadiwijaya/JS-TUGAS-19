3a1bebd9c83dfd55c8ce47709807ad2413d27c04
jauhhari <ervanadiwijaya@gmail.com>
2021-07-01 08:45:36 +0700

    ok

---
+++ tugas19.js
@@ -0,0 +1,20 @@
+
+function cetak() {
+    var kal = "Semangat...";
+
+    console.log("Soal 1 : ");
+    console.log(kal.repeat(5));
+
+    console.log();
+
+    console.log("Soal 2 : ");
+    console.log("*****");
+    console.log("*****".repeat(4));
+    console.log("*****".repeat(7));
+    console.log("*****".repeat(10));
+    console.log("*****".repeat(7));
+    console.log("*****".repeat(4));
+    console.log("*****");    
+}
+
+cetak();
